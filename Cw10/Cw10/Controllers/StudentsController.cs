6731062e0045776f8fc721a5ec4715d70d73a827
s18849 <s18849@pjwstk.edu.pl>
2020-05-07 11:30:13 +0200

    dodanie entityframework i jego konfiguracja

---
+++ Cw10/Cw10/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw10.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok(new s18849Context().Student.ToList());
+        }
+    }
+}
